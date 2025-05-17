8cf1f86689bb54ef651cbbba055bd7ec11490ea7
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-05-17 10:44:50 +0200

    Armor_X7_Pro: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: ulefone Model: Armor X7 Pro Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Armor_X7_Pro.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Armor_X7_Pro-user \
+    omni_Armor_X7_Pro-userdebug \
+    omni_Armor_X7_Pro-eng
