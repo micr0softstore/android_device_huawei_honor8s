961fd89f1713809bf9ad33fe8289b5147f614bcf
Microsoft Store (Balazs) <micr0softstore@outlook.com>
2025-10-19 21:21:25 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/huawei/Honor_8s
+DEVICE_PATH := device/huawei/Honor_8S
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
