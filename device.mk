4cf21f7265afc6066abbaca96470f337ba6d5ceb
Microsoft Store (Balazs) <micr0softstore@outlook.com>
2025-10-21 17:27:06 +0200

    fastbootd: inital support

--- device.mk
+++ device.mk
@@ -6,3 +6,10 @@
 #
 
 LOCAL_PATH := device/huawei/kansas
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+PRODUCT_PRODUCT_PROPERTIES += \
+	ro.fastbootd.available=true
